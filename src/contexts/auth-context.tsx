e3df19effa71c3d981fc351269e71e2243fe7c45
Dolf Andringa <dolfandringa@gmail.com>
2023-05-08 22:06:03 +0200

    Add user info to the auth context

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -1,20 +1,41 @@
 import React, { createContext, ReactNode, useState } from "react";
-import { Button, Message } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import { getCookie, setCookie } from "typescript-cookie";
 import { UserForm } from "../components/user-form";
-import { PermissionDeniedError } from "../../exceptions";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 interface IContext {
   loggedIn: boolean;
   logout?: () => Promise<void>;
+  user?: User;
+}
+
+interface Profile {
+  data: {
+    id: string;
+    attributes: {
+      first_name: string;
+      last_name: string;
+      email: string;
+      username: string;
+    };
+  };
+}
+
+interface User {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
 }
 
 export const AuthContext = createContext<IContext>({ loggedIn: false });
 
 export const AuthProvider = (props: { children?: ReactNode }) => {
+  const [user, setUser] = useState<User | undefined>();
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
-  const [error, setError] = useState<string|undefined>();
+  const [error, setError] = useState<string | undefined>();
 
   const onLogin = async (email: string, password: string) => {
     try {
@@ -24,10 +45,21 @@ export const AuthProvider = (props: { children?: ReactNode }) => {
       await axios.post("http://127.0.0.1:8000/login/", form);
       setLoggedIn(true);
       setError(undefined);
-    } catch(error) {
+      const resp: AxiosResponse<Profile> = await axios.get(
+        "http://127.0.0.1:8000/profile/",
+        { withCredentials: true }
+      );
+      const user: User = {
+        ...resp.data.data.attributes,
+        id: resp.data.data.id,
+      };
+      console.log("Profile", resp);
+      setUser(user);
+      console.log("Set user to", user);
+    } catch (error) {
       console.error("Login failed", error);
       setLoggedIn(false);
-      setError("Login failed.")
+      setError("Login failed.");
     }
   };
 
@@ -36,7 +68,7 @@ export const AuthProvider = (props: { children?: ReactNode }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ loggedIn, logout }}>
+    <AuthContext.Provider value={{ loggedIn, logout, user }}>
       {loggedIn ? (
         <>
           <Button as="a" onClick={logout}>
